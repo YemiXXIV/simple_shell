ae353225eeb9078771b41cd9cb04d215ec9e2a1d
Mustapha <mustapha.yinusa@yahoo.com>
2023-12-15 12:23:16 +0100

    Will it work

--- to_readinput.c
+++ to_readinput.c
@@ -10,11 +10,17 @@ char *to_readinput(void)
 {
 	char *buffer = NULL;
 	size_t bufsize = 0;
+	size_t length = strlen(buffer);
 
 	if (getline(&buffer, &bufsize, stdin) == EOF)
 	{
 		free(buffer);
 		exit(EXIT_SUCCESS);
+	}
+
+	if (length > 0 && buffer[length - 1] == '\n')
+	{
+		buffer[length - 1] = '\0';
 	}
 
 	return (buffer);
